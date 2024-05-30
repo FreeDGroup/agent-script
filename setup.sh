750699ab48705f7ef1c50ff1712d14b9a77e3ef1
Joon Hwan 김준환 <xncbf12@gmail.com>
2024-05-30 22:49:03 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo chmod 777 /var/run/docker.sock
 
-sudo mkdir -p ~/.docker/cli-plugins/
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.27.1/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-sudo chmod +x ~/.docker/cli-plugins/docker-compose
+sudo mkdir -p /home/azureuser/.docker/cli-plugins/
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.27.1/docker-compose-linux-x86_64 -o /home/azureuser/.docker/cli-plugins/docker-compose
+sudo chmod +x /home/azureuser/.docker/cli-plugins/docker-compose
