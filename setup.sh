8df09a9f3075ec7896670eae7afdbdd24fa71903
Joon Hwan 김준환 <xncbf12@gmail.com>
2024-05-31 10:16:50 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y docker.io
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo chmod 777 /var/run/docker.sock
 
-sudo mkdir -p /home/azureuser/.docker/cli-plugins/
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.27.1/docker-compose-linux-x86_64 -o /home/azureuser/.docker/cli-plugins/docker-compose
-sudo chmod +x /home/azureuser/.docker/cli-plugins/docker-compose
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# sudo systemctl start docker
+# sudo systemctl enable docker
+# sudo chmod 777 /var/run/docker.sock
+
+# sudo mkdir -p /home/azureuser/.docker/cli-plugins/
+# sudo curl -SL https://github.com/docker/compose/releases/download/v2.27.1/docker-compose-linux-x86_64 -o /home/azureuser/.docker/cli-plugins/docker-compose
+# sudo chmod +x /home/azureuser/.docker/cli-plugins/docker-compose
